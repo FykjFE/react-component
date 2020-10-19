9f560f8b04967b2f1ebc54d8d7cfa28db3aae4b1
chenc <chenc@fengyuntec.com>
2020-10-19 11:48:53 +0800

    feat: 发布

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.config");
 const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
