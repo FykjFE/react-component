5bbbb76b72160739f23a98a7f87f10e15a2bba53
chenc <chenc@fengyuntec.com>
2020-10-22 08:58:34 +0800

    feat: 添加storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
--- components/button/Button.stories.tsx
+++ components/button/Button.stories.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react';
 
-import { Button } from '../components/button';
-import { ButtonProps } from '../components/button/Button';
+import { Button } from './index';
+import { ButtonProps } from './Button';
 
 export default {
   title: 'Example/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  argTypes: {},
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => <Button {...args}>测试</Button>;
